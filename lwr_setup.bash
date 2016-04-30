4b745d0b158e20d39ed4164454de7be4fda8e092
Antoine Hoarau <hoarau.robotics@gmail.com>
2016-04-30 13:29:58 +0200

    test new install script

--- lwr_setup.bash
+++ lwr_setup.bash
@@ -1,99 +1,138 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
-sudo apt-get install -y -qq curl
+function setBoolean() {
+  local v
+  if (( $# != 2 )); then
+     echo "Err: setBoolean usage" 1>&2; exit 1 ;
+  fi
 
-## ROS
+  case "$2" in
+    TRUE) v=true ;;
+    FALSE) v=false ;;
+    *) echo "Err: Unknown boolean value \"$2\"" 1>&2; exit 1 ;;
+   esac
+
+   eval $1=$v
+}
+ros=false
 ROS_DISTRO=indigo
-if [ $(lsb_release -cs) == "precise" ]; then ROS_DISTRO=hydro ;fi
-if [ $(lsb_release -cs) == "trusty" ]; then ROS_DISTRO=indigo ;fi
-if [ $(lsb_release -cs) == "vivid" ]; then ROS_DISTRO=jade ;fi
+orocos=false
+orocos29=false
+gazebo=false
+gazebo_version=""
+rtt_lwr=false
+rtt_lwr_extras=false
+conman=false
+ws_path=$HOME/lwr_ws
+ws_src=$ws_path/src
+ROS_DISTRO=indigo
 
-sudo sh -c "echo 'deb http://packages.ros.org/ros/ubuntu $(lsb_release -cs) main' > /etc/apt/sources.list.d/ros-latest.list"
-wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install -qq -y python-rosdep python-catkin-tools
-sudo apt-get install -qq -y ros-$ROS_DISTRO-catkin python-wstool
+for i in "$@"
+do
+case $i in
+    --ws_path=*)
+    ws_path="${i#*=}"
+    shift
+    ;;
+    --ros=*)
+    setBoolean ros "${i#*=}"
+    shift
+    ;;
+    --ros_distro=*)
+    ROS_DISTRO="${i#*=}"
+    shift
+    ;;
+    --orocos=*)
+    setBoolean orocos="${i#*=}"
+    shift
+    ;;
+    --orocos29=*)
+    setBoolean orocos29="${i#*=}"
+    shift
+    ;;
+    --gazebo=*)
+    setBoolean gazebo="${i#*=}"
+    shift
+    ;;
+    --gazebo_version=*)
+    setBoolean gazebo_version="${i#*=}"
+    shift
+    ;;
+    --rtt_lwr=*)
+    setBoolean rtt_lwr="${i#*=}"
+    shift
+    ;;
+    --rtt_lwr_extras=*)
+    setBoolean rtt_lwr_extras="${i#*=}"
+    shift
+    ;;
+    --conman=*)
+    setBoolean conman="${i#*=}"
+    shift
+    ;;
+    --default)
+    DEFAULT=YES
+    shift
+    ;;
+    *)
 
-#ROS
-source /opt/ros/$ROS_DISTRO/setup.bash
-## Rosdep
-sudo rosdep init
-rosdep update
+    ;;
+esac
+done
 
-
-##### Creating user directories
-if [ ! -n "$ROS_WS" ]; then
-	ROS_WS=/home/$USER
+if [[ $ros ]] ; then
+    echo "Installing ROS Indigo" $ros
+    sudo sh -c "echo 'deb http://packages.ros.org/ros/ubuntu $(lsb_release -cs) main' > /etc/apt/sources.list.d/ros-latest.list"
+    wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
+    sudo apt-get update
+    sudo apt-get install -qq -y python-rosdep python-catkin-tools
+    sudo apt-get install -qq -y ros-$ROS_DISTRO-catkin python-wstool
+    source /opt/ros/$ROS_DISTRO/setup.bash
+    sudo rosdep init
+    rosdep update
 fi
 
-if [ ! -n "$LWR_WS" ]; then
-	LWR_WS=$ROS_WS/lwr_ws
+if [[ $orocos ]] ; then
+    echo "Installting OROCOS 2.8 from debians"
+    sudo apt-get install ros-$ROS_DISTRO-orocos-toolchain ros-$ROS_DISTRO-rtt-*
+elif $orocos29 ; then
+    echo "Installting OROCOS 2.9 from source"
+    mkdir -p $HOME/orocos_ws/src
+    cd $HOME/orocos_ws
+    catkin init
+    catkin config --install
+    sudo apt-get install -y git
+    cd $HOME/orocos_ws/src
+    git clone https://github.com/orocos-toolchain/orocos_toolchain.git --recursive -b toolchain-2.9
+    catkin build
+    source $HOME/orocos_ws/install/setup.bash
 fi
 
-mkdir -p $LWR_WS/src
-#################################
-
-cd $LWR_WS/src
-
-cd $LWR_WS
-catkin init
-
-cd $LWR_WS/src
-wstool init
-
-curl https://raw.githubusercontent.com/kuka-isir/rtt_lwr/rtt_lwr-2.0/lwr_utils/config/rtt_lwr.rosinstall | wstool merge -
-curl https://raw.githubusercontent.com/kuka-isir/rtt_lwr/rtt_lwr-2.0/lwr_utils/config/rtt_lwr_extras.rosinstall | wstool merge -
-
-## Download source
-if [ -n "$TRAVIS" ];then
-	wstool update -j2
-else
-	wstool update -j$(nproc)
+if [[ $gazebo ]]; then
+    sudo apt-get install -y ros-$ROS_DISTRO-gazebo$gazebo_version-*
 fi
 
-curl https://raw.githubusercontent.com/IDSCETHZurich/re_trajectory-generator/master/kuka_IK/include/friComm.h >> $LWR_WS/src/rtt_lwr/lwr_hardware/kuka_lwr_fri/include/kuka_lwr_fri/friComm.h
+if [[ $rtt_lwr ]]; then
+    sudo apt-get install -y curl
+    mkdir -p $ws_src
+    cd ~/lwr_ws/
+    catkin init
+    cd ~/lwr_ws/src
+    wstool init
+    curl https://raw.githubusercontent.com/kuka-isir/rtt_lwr/rtt_lwr-2.0/lwr_utils/config/rtt_lwr.rosinstall | wstool merge -
 
-## Warning this installs gazebo 2 (default in ros indigo, so please run this before installing gazebo 6 (below))
-rosdep install -r --from-paths $LWR_WS/ --rosdistro $ROS_DISTRO -y
+    if [[ $rtt_lwr_extras ]]; then
+        curl https://raw.githubusercontent.com/kuka-isir/rtt_lwr/rtt_lwr-2.0/lwr_utils/config/rtt_lwr_extras.rosinstall | wstool merge -
+    fi
 
+    if [[ $conman ]]; then
+        curl https://raw.githubusercontent.com/kuka-isir/rtt_lwr/rtt_lwr-2.0/lwr_utils/config/conman.rosinstall | wstool merge -
+    fi
 
-######### INSTALL GAZEBO 6 ###############################################
-if [ -n "$GAZEBO6" ]; then
-sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
-wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
-sudo apt-get update
-
-sudo apt-get -y install libsdformat3 sdformat-sdf
-sudo apt-get -y install gazebo6
-# For developers that work on top of Gazebo, one extra package
-sudo apt-get -y install libgazebo6-dev
-sudo apt-get -y install ros-$ROS_DISTRO-gazebo6-*
+    wstool update -j$(nproc)
+    rosrun rtt_roscomm create_rtt_msgs control_msgs
+    rosrun rtt_roscomm create_rtt_msgs controller_manager_msgs
+    curl https://raw.githubusercontent.com/IDSCETHZurich/re_trajectory-generator/master/kuka_IK/include/friComm.h >> $ws_src/rtt_lwr/lwr_hardware/kuka_lwr_fri/include/kuka_lwr_fri/friComm.h
+    catkin build
+    source $ws_path/devel/setup.bash
 fi
-#########################################################################
-
-######### INSTALL GAZEBO 7 ###############################################
-
-sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
-wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
-sudo apt-get update
-
-sudo apt-get -y install libsdformat4 sdformat-sdf
-sudo apt-get -y install gazebo7
-# For developers that work on top of Gazebo, one extra package
-sudo apt-get -y install libgazebo7-dev
-sudo apt-get -y install ros-$ROS_DISTRO-gazebo7-*
-
-#########################################################################
-
-
-
-# Build everything
-cd $LWR_WS/src
-if [ -n "$TRAVIS" ]; then
-    catkin build --limit-status-rate 0.1 --no-notify --no-status -j2 -DCATKIN_ENABLE_TESTING=OFF -DCMAKE_BUILD_TYPE=Debug
-else
-    catkin build -DCATKIN_ENABLE_TESTING=OFF -DCMAKE_BUILD_TYPE=Release
-fi
-
-source $LWR_WS/devel/setup.sh
